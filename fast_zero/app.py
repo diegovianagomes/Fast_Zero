917e8f637d313e9e9a9463269e88d504de3923e9
dvgba <diegovianagomes01@gmail.com>
2024-07-02 16:15:14 -0300

    Aula 02

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,15 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
 
+from fast_zero.schemas import Message
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'message': 'Hello World'}  # noqa: F821
+    return {'message': 'Hello World', 'batata': 'frita'}
+
+@app.get('/potato', status_code=HTTPStatus.OK, response_model=Message)
+def read_potato():
+    return {'message': 'batata frita'}
